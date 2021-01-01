cf1b0f841f1149eae3f09d552360424e3a0ceaad
Matthew Grigajtis <matthew.grigajtis@gmail.com>
2021-01-01 16:59:46 -0500

    Adds javascript

--- holidaylights.js
+++ holidaylights.js
@@ -1,6 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
   function generateHeartsOnClick() {
-	// Animated hearts to delight the user
+    function generateHeart(x, y, xBound, xStart, scale) {
+        var heart = document.createElement('div');
+        heart.setAttribute('class', 'heart');
+        brd.appendChild(heart);
+        heart.time = duration;
+        heart.x = x;
+        heart.y = y;
+        heart.bound = xBound;
+        heart.direction = xStart;
+        heart.style.left = heart.x + 'px';
+        heart.style.top = heart.y + 'px';
+        heart.scale = scale;
+        heart.style.transform = 'scale(' + scale + ',' + scale + ')';
+        if (hearts === null)
+            hearts = [];
+        hearts.push(heart);
+        return heart;
+    }
+
+    function frame() {
+        var current = Date.now();
+        var deltaTime = current - before;
+        before = current;
+        for (i in hearts) {
+            var heart = hearts[i];
+            heart.time -= deltaTime;
+            if (heart.time > 0) {
+                heart.y -= speed;
+                heart.style.top = heart.y + 'px';
+                heart.style.left = heart.x + heart.direction * heart.bound * Math.sin(heart.y * heart.scale / 30) / heart.y * 100 + "px";
+            }
+            else {
+                heart.parentNode.removeChild(heart);
+                hearts.splice(i, 1);
+            }
+        }
+    }
+
+    function check() {
+        if (down) {
+            var start = 1 - Math.round(Math.random()) * 2;
+            var scale = Math.random() * Math.random() * 0.8 + 0.2;
+            var bound = 30 + Math.random() * 20;
+            generateHeart(event.pageX - brd.offsetLeft + cursorXOffset, event.pageY - brd.offsetTop + cursorYOffset, bound, start, scale);
+        }
+    }
         var brd = document.createElement('div');
         //document.body.insertBefore(brd, document.getElementById('hFF'));
 
@@ -53,54 +98,9 @@ document.addEventListener('DOMContentLoaded', function() {
         var id = setInterval(frame, 5);
         var gr = setInterval(check, 100);
 
-        function generateHeart(x, y, xBound, xStart, scale) {
-            var heart = document.createElement('div');
-            heart.setAttribute('class', 'heart');
-            brd.appendChild(heart);
-            heart.time = duration;
-            heart.x = x;
-            heart.y = y;
-            heart.bound = xBound;
-            heart.direction = xStart;
-            heart.style.left = heart.x + 'px';
-            heart.style.top = heart.y + 'px';
-            heart.scale = scale;
-            heart.style.transform = 'scale(' + scale + ',' + scale + ')';
-            if (hearts === null)
-                hearts = [];
-            hearts.push(heart);
-            return heart;
-        }
 
-        function frame() {
-            var current = Date.now();
-            var deltaTime = current - before;
-            before = current;
-            for (i in hearts) {
-                var heart = hearts[i];
-                heart.time -= deltaTime;
-                if (heart.time > 0) {
-                    heart.y -= speed;
-                    heart.style.top = heart.y + 'px';
-                    heart.style.left = heart.x + heart.direction * heart.bound * Math.sin(heart.y * heart.scale / 30) / heart.y * 100 + "px";
-                }
-                else {
-                    heart.parentNode.removeChild(heart);
-                    hearts.splice(i, 1);
-                }
-            }
-        }
-
-        function check() {
-            if (down) {
-                var start = 1 - Math.round(Math.random()) * 2;
-                var scale = Math.random() * Math.random() * 0.8 + 0.2;
-                var bound = 30 + Math.random() * 20;
-                generateHeart(event.pageX - brd.offsetLeft + cursorXOffset, event.pageY - brd.offsetTop + cursorYOffset, bound, start, scale);
-            }
-        }
   }
-  
+
   function putUpChristmasLights() {
     var div = document.createElement('div');
     div.id = 'christmasLights';
